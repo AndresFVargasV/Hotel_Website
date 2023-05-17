af385520ab5bbd5d9ed69c0e92c4cd905058a77c
Andres Felipe Vargas Vasquez <AndresFVargas10@outlook.com>
2023-05-16 20:17:53 -0500

    BD creada

--- source_page/app.js
+++ source_page/app.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const sql = require('mssql');
-const config = require('./config');
+const config = require('./config.js');
 const morgan = require('morgan');
 
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('wiew engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-//conexion a la base de datos
-async function connectToDatabase() {
-  try {
-    await sql.connect(config);
-    console.log('Conexión exitosa a SQL Server');
-  } catch (error) {
-    console.log('Error al conectar a SQL Server:', error);
-  }
-}
-connectToDatabase();
 
 //middlewares
 app.use(morgan('dev'));
--- source_page/config.js
+++ source_page/config.js
@@ -1,14 +1,26 @@
-const sql = require('mssql/msnodesqlv8');
+const sql = require('mssql');
 
-var config = {
-   database: 'hotel',
-  server: 'ANDRESFVARGAS\\SQLEXPRESS',
-  driver: 'msnodesqlv8',
-  options: {
-    trustedConnection: true
-  }
+const config = {
+  user: 'andres-sa',
+  password: '123456789db*',
+  server: 'andres-databases.database.windows.net',
+  database: 'bd_hotel',
 };
-module.exports = config;
+
+async function getReservas() {    
+  try {
+    await sql.connect(config);
+    const result = await sql.query('SELECT * FROM Reservas');
+  } catch (error) {
+    console.log(error);
+  } finally {
+    sql.close();
+  }
+}
+
+getReservas();
+
+
 
 
 
