45d9a1919851078cef580376bc7b5434283e98d0
Andres Felipe Vargas Vasquez <AndresFVargas10@outlook.com>
2023-05-17 21:40:18 -0500

    Modulo reserva

--- source_page/app.js
+++ source_page/app.js
@@ -1,20 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const config = require('./config.js');
+const { pool, dbConnect, verificarDisponibilidad, hacerReserva } = require('./config');
 const morgan = require('morgan');
+
+
 
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('wiew engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 //middlewares
 app.use(morgan('dev'));
 
+
 //routes
 
-const port = 3000;
+app.use(express.json());
+
+// Ruta para manejar la solicitud de reserva
+app.post('/ruta-de-reserva', (req, res) => {
+  const reservaCliente = req.body;
+  // Aquí puedes procesar los datos y hacer la reserva en la base de datos
+  // ...
+  hacerReserva(reservaCliente);
+
+  // Enviar una respuesta al cliente
+  res.send('Reserva realizada exitosamente.');
+});
+// Fin de los datos de una reserva del cliente
+
+
+
+//puerto escuchando
+const port = 5500;
 app.listen(app.get('port'), () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
+
